cfed839b663ec9005e18a8022569d6002d50ff65
yutisio12 <yutisiogamings@gmail.com>
2026-07-10 20:54:52 +0700

    TEST UNITS TEMPLATE

--- template/src/test/kotlin/com/sepring/template/TemplateApplicationTests.kt
+++ template/src/test/kotlin/com/sepring/template/TemplateApplicationTests.kt
@@ -2,21 +2,10 @@ package com.sepring.template
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
-@TestPropertySource(properties = [
-    "spring.datasource.url=jdbc:h2:mem:test_db",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=",
-    "spring.datasource.driver-class-name=org.h2.Driver",
-    "app.datasource.secondary.url=jdbc:h2:mem:test_secondary_db",
-    "app.datasource.secondary.username=sa",
-    "app.datasource.secondary.password=",
-    "app.datasource.secondary.driver-class-name=org.h2.Driver"
-])
 class TemplateApplicationTests {
-	@Test
+	@Test. 
 	fun contextLoads() {
 	}
 
