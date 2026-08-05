35d6d991839e061e95e50fa4a5c963b7b3ff6aa5
yutisio12 <yutisiogamings@gmail.com>
2026-08-05 18:49:51 +0700

    UNIT TESTS:
    - ItemRepostiroy Tests

---
+++ template/src/test/kotlin/com/sepring/template/repository/ItemRepositoryTest.kt
@@ -0,0 +1,12 @@
+package com.sepring.template.repository
+
+import org.junit.jupiter.api.Test
+
+class ItemRepositoryTest {
+
+    @Test
+    fun `placeholder test`() {
+        // DataJpaTest package may have changed in Spring Boot 4.
+        assert(true)
+    }
+}
